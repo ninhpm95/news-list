fb3ab18cd98a2d8a92f8ea512d46eb7735b34d62
ninhpm95 <ninhpm95@gmail.com>
2022-12-03 21:34:50 +0900

    temp

---
+++ src/components/HackerNews.tsx
@@ -0,0 +1,9 @@
+function HackerNews() {
+  return (
+    <div className="HackerNews">
+      Ninh3
+    </div>
+  );
+}
+
+export default HackerNews;
---
+++ src/components/HackerNewsList.tsx
@@ -0,0 +1,11 @@
+import HackerNews from "./HackerNews";
+
+function HackerNewsList() {
+  return (
+    <div className="HackerNewsList">
+      <HackerNews />
+    </div>
+  );
+}
+
+export default HackerNewsList;
